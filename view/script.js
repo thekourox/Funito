97a5e66519816340c45d569f329fb6bb533a1f40
Kornik <hackedbykornik@gmail.com>
2025-01-14 16:21:15 +0330

    fal database completed

--- view/script.js
+++ view/script.js
@@ -2,19 +2,29 @@ async function getFal() {
     try {
         const response = await fetch('http://localhost:3000/api/fal');
         const data = await response.json();
-        document.getElementById('falTitle').innerHTML = data.title;
-        document.getElementById('falText').innerHTML = data.interpreter;
-        document.getElementById('getFal').style.display = 'none';
-        document.getElementById('backButton').style.display = 'inline-block';
-        document.getElementById('backButton').style.background = '#3291fc';
+        setTimeout(() => {
+            document.getElementById('waitText').style.display = 'none';
+            document.getElementById('falTitle').innerHTML = data.title;
+            document.getElementById('falText').innerHTML = data.interpreter;
+            document.getElementById('getFal').style.display = 'none';
+            document.getElementById('backButton').style.display = 'inline-block';
+            document.getElementById('backButton').style.background = '#3291fc';
+        }, 1500);
     } catch (err) {
         if (err) {
             setTimeout(() => {
-                alert("خطا در برقراری ارتباط با سرور");
+                
+                document.getElementById('waitText').innerHTML = "خطا در برقراری ارتباط با سرور !"
             }, 4000);
         }
     }
 }
+
+document.getElementById('getFal').addEventListener('click', () => {
+    document.getElementById('waitText').style.display = 'inline-block';
+    document.getElementById('waitText').innerHTML = "لطفا صبر نمایید ...";
+    document.getElementById('footer').style.display = 'inline-block';
+})
 
 document.getElementById('backButton').addEventListener('click', () => {
     document.getElementById('falTitle').innerHTML = null;
